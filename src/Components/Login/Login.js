05e4e31ddb1695547ebf99fd4f7f691c13ab0232
RAHUL RUDRA <rahulrudra245@gmail.com>
2022-05-16 15:50:40 +0600

    authentication2 commit

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -2,19 +2,52 @@ import React from "react";
 import { useSignInWithGoogle } from "react-firebase-hooks/auth";
 
 import auth from "../Firebase/FirebaseInit";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
   const [signInWithGoogle, guser, gloading, gerror] = useSignInWithGoogle(auth);
 
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+
   if (guser) {
     console.log(guser);
   }
-
+  const onSubmit = (data) => console.log(data);
   return (
     <div className="flex  h-screen justify-center items-center">
       <div class="card w-96 bg-base-100 shadow-xl">
         <div class="card-body">
           <h2 class=" text-center text-2xl ">Login!</h2>
+
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div class="form-control w-full max-w-xs">
+              <label class="label">
+                <span class="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="Email here"
+                class="input input-bordered w-full max-w-xs"
+                {...register("email", {
+                  pattern: {
+                    value: /[A-Za-z]{3}/,
+                    message: "error message", // JS only: <p>error message</p> TS only support string
+                  },
+                })}
+              />
+            </div>
+            <input {...register("firstName", { required: true })} />
+            {errors.firstName?.type === "required" && "First name is required"}
+
+            <input />
+            {errors.lastName && "Last name is required"}
+
+            <input type="submit" />
+          </form>
 
           <div class="divider">OR</div>
           <button onClick={() => signInWithGoogle()} class="btn btn-info">
