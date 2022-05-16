1787f2d9755b78839f9a4a9a7da642693fb885dd
RAHUL RUDRA <rahulrudra245@gmail.com>
2022-05-16 15:25:16 +0600

    authentication commit

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+
+import auth from "../Firebase/FirebaseInit";
 
 const Login = () => {
+  const [signInWithGoogle, guser, gloading, gerror] = useSignInWithGoogle(auth);
+
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="flex  h-screen justify-center items-center">
+      <div class="card w-96 bg-base-100 shadow-xl">
+        <div class="card-body">
+          <h2 class=" text-center text-2xl ">Login!</h2>
+
+          <div class="divider">OR</div>
+          <button class="btn btn-info">Continue With Google</button>
+        </div>
+      </div>
     </div>
   );
 };
