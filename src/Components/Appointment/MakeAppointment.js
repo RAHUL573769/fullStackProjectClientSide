0b4fa32e57a7d08cc504b67539f2a8cab78c6721
RAHUL RUDRA <rahulrudra245@gmail.com>
2022-05-16 09:09:27 +0600

    changes11 commit

--- src/Components/Appointment/MakeAppointment.js
+++ src/Components/Appointment/MakeAppointment.js
@@ -7,7 +7,17 @@ const MakeAppointment = ({ service }) => {
       <div class="card card-compact w-96 bg-base-100 shadow-xl lg:max-w-lg">
         <div class="card-body">
           <h2 class="card-title">{service.name}</h2>
-          <p>{slots.length} spaces available</p>
+
+          <p>
+            {slots.length > 0 ? (
+              <span>{slots[0]} </span>
+            ) : (
+              <span className="text-red-500">No slots Available</span>
+            )}
+          </p>
+          <p>
+            {slots.length} {slots.length > 1 ? "slots" : "slot"} available
+          </p>
           <div class="card-actions justify-end">
             <button class="btn btn-primary">Buy Now</button>
           </div>
